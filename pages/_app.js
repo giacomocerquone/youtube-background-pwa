cf6e837abafd9cefb654bbb0d3baa987b42503c7
gc <cerquone96@hotmail.it>
2022-02-23 23:30:09 +0100

    debug

--- pages/_app.js
+++ pages/_app.js
@@ -17,18 +17,8 @@ function MyApp({ Component, pageProps }) {
         <title>YouTube background</title>
 
         <link rel="manifest" href="/manifest.json" />
-        <link
-          href="/icons/favicon-16x16.png"
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-        />
-        <link
-          href="/icons/favicon-32x32.png"
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-        />
+        <link href="/favicon.ico" rel="icon" type="image/png" sizes="16x16" />
+        <link href="/favicon.ico" rel="icon" type="image/png" sizes="32x32" />
         <link rel="apple-touch-icon" href="/apple-icon.png"></link>
         <meta name="theme-color" content="#317EFB" />
       </Head>
